17d97c0f64b525b9b99fb8e7b796f56e2356398d
aidancordero2 <aidancordero2@gmail.com>
2025-12-16 11:25:09 -0500

    update to version 0.1.7

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ project = "GaugeFixer"
 author = "Carlos Martí-Gómez, Justin B. Kinney"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.1.7"
 
 
 # -- General configuration ---------------------------------------------------
